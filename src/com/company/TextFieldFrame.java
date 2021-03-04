b0d644f0bd5f6755fefb8c0573bbd0d7be86ec63
Luana Franca <21898@student.dorset-college.ie>
2021-03-04 19:53:46 +0000

    Second Commit

--- src/com/company/TextFieldFrame.java
+++ src/com/company/TextFieldFrame.java
@@ -21,13 +21,23 @@ public class TextFieldFrame extends JFrame {
         textField1 = new JTextField(10);
         add(textField1);
 
-        textField2 = null;
-        textField3 = null;
-        passwordField = null;
+        textField2 = new JTextField("Enter text here");
+        add(textField2);
+
+        textField3 = new JTextField("Cannot be edited", 21);
+        textField3.setEditable(false);
+        add(textField3);
+
+        passwordField = new JPasswordField("Hidden Text");
+        add(passwordField);
+
 
         TextFieldHandler handler = new TextFieldHandler();
 
         textField1.addActionListener(handler);
+        textField2.addActionListener(handler);
+        textField3.addActionListener(handler);
+        passwordField.addActionListener(handler);
 
     }
 
@@ -37,6 +47,31 @@ public class TextFieldFrame extends JFrame {
         public void actionPerformed(ActionEvent event) {
 
             String message = "Not yet handled";
+
+            if (event.getSource() == textField1) {
+
+                message = String.format("texfield1: %s", event.getActionCommand());
+
+            }
+
+            else if (event.getSource() == textField2) {
+
+                message = String.format("texfield2: %s", event.getActionCommand());
+
+            }
+
+            else if (event.getSource() == textField3) {
+
+                message = String.format("texfield3: %s", event.getActionCommand());
+
+            }
+
+            else if (event.getSource() == passwordField) {
+
+                message = String.format("passwordField: %s", event.getActionCommand());
+
+            }
+
             JOptionPane.showMessageDialog(null, message);
 
         }
