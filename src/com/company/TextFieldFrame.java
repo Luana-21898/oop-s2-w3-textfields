f81d26d903949183f3f39163e0ac3af0b5c4e200
Luana Franca <21898@student.dorset-college.ie>
2021-03-04 19:32:37 +0000

    First Commit

---
+++ src/com/company/TextFieldFrame.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextFieldFrame extends JFrame {
+
+    private final JTextField textField1;
+    private final JTextField textField2;
+    private final JTextField textField3;
+    private final JPasswordField passwordField;
+
+
+    public TextFieldFrame() {
+
+        super("Text Field Demonstration with Events");
+        setLayout(new FlowLayout());
+
+        textField1 = new JTextField(10);
+        add(textField1);
+
+        textField2 = null;
+        textField3 = null;
+        passwordField = null;
+
+        TextFieldHandler handler = new TextFieldHandler();
+
+        textField1.addActionListener(handler);
+
+    }
+
+    private class TextFieldHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+
+            String message = "Not yet handled";
+            JOptionPane.showMessageDialog(null, message);
+
+        }
+
+    }
+}
